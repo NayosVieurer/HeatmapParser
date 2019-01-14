cff139fceeead22c57436e86ea5101b550698b63
MINARDI Morgan <m.minardi@rubika-edu.com>
2019-01-14 19:33:46 +0100

    heatmap path setting system

--- HeatmapParserWPF/MainWindow.xaml.cs
+++ HeatmapParserWPF/MainWindow.xaml.cs
@@ -1,23 +1,26 @@
 ﻿#pragma warning disable MissingOptionsRule // Missing Or Corrupted Options
 using System.Windows;
+using System.Windows.Input;
 using System.ComponentModel;
+using System.IO;
 using System;
 
-public struct HeatPoint
-{
-    public int X;
-    public int Y;
-    public byte intensity;
-    public HeatPoint(int iX, int iY, byte intens)
-    {
-        X = iX;
-        Y = iY;
-        intensity = intens;
-    }
-}
 
 namespace HeatmapParserWPF
 {
+
+    public struct HeatPoint
+    {
+        public int X;
+        public int Y;
+        public byte intensity;
+        public HeatPoint(int iX, int iY, byte intens)
+        {
+            X = iX;
+            Y = iY;
+            intensity = intens;
+        }
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -25,21 +28,24 @@ namespace HeatmapParserWPF
     {
         bool isMaximised;
 
+        string path = "X:\\J002\\01_PERSO\\BLACK_HIVE\\Playtest_Datas\\Heatmaps";
+
         public MainWindow()
         {
             InitializeComponent();
+
+                
 
             isMaximised = Properties.Settings.Default.Maximized;
             this.WindowState = isMaximised ? WindowState.Maximized : WindowState.Normal;
 
             
+
         }
 
         private void Window_Closing(object sender, CancelEventArgs e)
         {
             Properties.Settings.Default.Maximized = isMaximised;
-
-            
 
             Properties.Settings.Default.Save();
         }
@@ -51,6 +57,51 @@ namespace HeatmapParserWPF
                 isMaximised = !isMaximised;
             }
         }
+
+        private void ExitCommand_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private void ExitCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            Console.WriteLine("toto");
+
+            if (!Directory.Exists(path))         
+            {
+                InputDialog dialog;
+
+                do
+                {
+                    dialog = new InputDialog("The heatmap folder was not found. Please enter a valid path : ");
+
+                    if (dialog.ShowDialog() == true)
+                    {
+                        path = dialog.answer;
+                    }
+                } while (!Directory.Exists(path));
+            }
+        }
+    }
+
+
+    public static class CustomCommands
+    {
+        public static readonly RoutedUICommand Exit = new RoutedUICommand
+        (
+            "Exit",
+            "Exit",
+            typeof(CustomCommands),
+            new InputGestureCollection()
+            {
+                new KeyGesture(Key.F4, ModifierKeys.Alt)
+            }
+        );
     }
 }
 
---
+++ HeatmapParserWPF/PathInput.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HeatmapParserWPF
+{
+    /// <summary>
+    /// Interaction logic for PathInput.xaml
+    /// </summary>
+    public partial class InputDialog : Window
+    {
+        public string answer { get { return inputAnswer.Text; } }
+
+        public InputDialog(string question, string defaultAnswer = "")
+        {
+            InitializeComponent();
+            dialQuestion.Content = question;
+            inputAnswer.Text = defaultAnswer;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            inputAnswer.Focus();
+        }
+
+        private void DialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+    }
+}
