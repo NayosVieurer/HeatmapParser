eb25331c7ce13f15146ed9f4e70e82416cb7e634
MINARDI Morgan <m.minardi@rubika-edu.com>
2019-03-19 19:01:33 +0100

    Creation of a file to store enums

---
+++ HeatmapParserWPF/Enums.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeatmapParserWPF
+{
+    public enum MapType
+    {
+        Steps,
+        Creature,
+        Soldier
+    }
+}
